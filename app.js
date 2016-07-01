56fc557f9a0d45b517ba1e70f88c468a642b2f3d
Janis Krasemann <krasemann.janis@gmail.com>
2016-07-01 22:01:41 +0200

    Improve architecture

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-const Editor = React.createClass({
-  getInitialState: function () {
-    return { value: '' };
-  },
-  handleChange: function (event) {
-    const text = event.target.value;
-    this.setState({ value: text });
-  },
-  render: function () {
-    return (
-      <div>
-        <textarea
-          placeholder='your text here'
-          onChange={this.handleChange} />
-        <div dangerouslySetInnerHTML={{__html: marked(this.state.value)}}>
-        </div>
-      </div>
-    );
-  }
-});
+const defaultText = '*italic*, **bold**, `monospace`, ~~strikethrough~~ .';
+
+const Editor = ({ handleChange }) => (
+  <textarea
+    className='editor'
+    placeholder='your text here'
+    onChange={handleChange} />
+);
+
+const Preview = ({ markdown }) => (
+  <div
+    className='preview'
+    dangerouslySetInnerHTML={{__html: markdown}} />
+);
 
 const App = React.createClass({
+  getInitialState: function () {
+    return {
+      text: defaultText,
+      markdown: marked(defaultText)
+    };
+  },
+  handleChange: function (event) {
+    const t = event.target.value;
+    this.setState({ text: t, markdown: marked(t) });
+  },
   render: function () {
     return (
       <div>
-        <h1>Yet another markdown live preview thingy...</h1>
-        <Editor />
+        <h1>Markdown Live Preview</h1>
+        <Editor handleChange={this.handleChange} />
+        <Preview markdown={this.state.markdown} />
       </div>
     );
   }
